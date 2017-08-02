dfdfc345e7cf7e70b2bbf51cd0184763b1f77d86
Michael Taylor <CoolDadTx@yahoo.com>
2017-08-01 22:17:34 -0500

    Added new files

---
+++ RestApiProjectTemplate/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace $safeprojectname$.Controllers
+{
+    [RoutePrefix("api/values")]
+    public class ValuesController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public Task<DateTime> Get ()
+        {
+            return Task.FromResult(DateTime.Now);
+        }
+    }
+}
